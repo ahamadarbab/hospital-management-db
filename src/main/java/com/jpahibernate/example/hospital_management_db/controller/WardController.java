927c725c6a5bab323696c3602e5f4c8335453c4f
ahamadarbab <ahmadarbab.456@gmail.com>
2026-02-08 23:59:03 +0530

    Add save API for ward

--- src/main/java/com/jpahibernate/example/hospital_management_db/controller/WardController.java
+++ src/main/java/com/jpahibernate/example/hospital_management_db/controller/WardController.java
@@ -1,4 +1,23 @@
 package com.jpahibernate.example.hospital_management_db.controller;
 
+import com.jpahibernate.example.hospital_management_db.model.Ward;
+import com.jpahibernate.example.hospital_management_db.service.WardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ward/apis")
 public class WardController {
+
+    @Autowired
+    WardService wardService;
+
+    @PostMapping("/save")
+    public String saveWard(@RequestBody Ward ward) {
+        String response = wardService.addWard(ward);
+        return response;
+    }
 }
